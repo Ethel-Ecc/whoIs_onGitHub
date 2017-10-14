7d4127adaf0c5273e7bf61066a7c7f67eba60fb5
Ethel <ethelbert@ethel.consulting.com>
2017-10-14 19:28:49 +0200

    completed the ajax functionaliy

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
 $(document).ready(function(){
-	//console.log('Ready to fly...');
-	$('#searchUser').on('keyup', function(){
-		console.log('You made an entry');
+	$('#searchUser').on('keyup', function(e){
+		let username = e.target.value;
+
+		// This will make the AJAX request to the GitHUB API url. 
+		// there are lot of diiferent functions to use, but $.ajax is nice. 
+		// It will take in an object(also called a dictionary) with our parameters[url and data]
+		$.ajax({
+			url: 'https://api.github.com/users/'+username,
+			data: {
+				client_id:'cdbdc6513ffbe63a46f2',
+				client_secret:'74642d4319697d76b3390dfe0dedc1bdece4cbea'
+			}
+		}).done(function (user){  
+				$('#profile').html(`
+					<div class="panel panel-default" style='margin-top:20px'>
+					  <div class="panel-heading">
+					    <h3 class="panel-title">${user.name}</h3>
+					  </div>
+					  <div class="panel-body">
+					   <div class = 'row'>
+
+					   		<div class= "col-md-3">
+						   		<img class="thumbnail avatar" src="${user.avatar_url}">
+						   		<a class="btn btn-success btn-block shadow" target="_blank" href='${user.html_url}'>View profile</a>
+					   		</div>
+
+					   		<div class='col-md-9'>
+					   			<span class="label label-default">Public Repos: ${user.public_repos}</span>
+								<span class="label label-primary">Public Gists: ${user.public_gists}</span>
+								<span class="label label-success">Followers: ${user.followers}</span>
+								<span class="label label-info">Folowing: ${user.following}</span>
+								<br><br>
+								<ul class="list-group">
+									<li class="list-group-item">Company: ${user.company}</li>
+									<li class="list-group-item">Website/Blog: ${user.blog}</li>
+									<li class="list-group-item">Location: ${user.location}</li>
+									<li class="list-group-item">Member Since: ${user.created_at}</li>
+								</ul>
+					   		</div>
+					   </div>
+					  </div>
+					</div>
+
+					<h3 class='page-header'>Latest Repositories</h3>
+					`);
+		});
+
+
+
 	});
 });
